0453d5d8e8cad9eb3b0e754caf10abbaf4b7702f
Pablo <pmiguelferrer@gmail.com>
2026-04-02 13:31:23 +0100

    add rabbit and logic stocl

--- src/InventoryService/Program.cs
+++ src/InventoryService/Program.cs
@@ -1,7 +1,34 @@
-using InventoryService;
+using InventoryService.Consumers;
+using MassTransit;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/inventory-.log", rollingInterval: RollingInterval.Day)
+    .Enrich.WithProperty("ServiceName", "InventoryService")
+    .CreateLogger();
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddSerilog();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderSubmittedConsumer>();
+
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"] ?? "localhost", "/", h =>
+        {
+            h.Username(builder.Configuration["RabbitMQ:User"] ?? "guest");
+            h.Password(builder.Configuration["RabbitMQ:Password"] ?? "guest");
+        });
+
+        cfg.ConfigureEndpoints(ctx);
+    });
+});
 
 var host = builder.Build();
-host.Run();
+
+Log.Information("InventoryService starting...");
+
+await host.RunAsync();
