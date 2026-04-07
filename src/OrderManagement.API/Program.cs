e4e81c0389991c86d015bf0debbf0188d8738fd7
Pablo <pmiguelferrer@gmail.com>
2026-04-07 14:54:12 +0100

    fix: use EnsureDeleted/EnsureCreated for DB to avoid migration conflict

--- src/OrderManagement.API/Program.cs
+++ src/OrderManagement.API/Program.cs
@@ -52,7 +52,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
 
     if (!db.Products.Any())
     {
